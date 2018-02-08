d3d8e2c51658de9b04f0b84d752fb79143cd1537
sean9392 <seancls@hotmail.co.uk>
2018-02-08 15:04:27 +0000

    fek

---
+++ NeonCrisis/Assets/Play_pew.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_pew : MonoBehaviour {
+
+	public Animator Pew;
+
+	// Use this for initialization
+	void Start () {
+
+		Pew.GetComponent<Animator> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("p")){
+			if (Pew != null) {
+				print ("BLOOP");
+			}
+			Pew.SetTrigger ("Shoot");
+
+
+			}
+		
+
+
+		
+
+	}
+
+}
